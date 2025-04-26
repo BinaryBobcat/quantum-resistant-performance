4dae7db3cf387e52c3a499770ce6f150bff4f3c8
rypizzo <81832261+rypizzo@users.noreply.github.com>
2025-04-26 13:06:58 -0400

    Update sphincs.py

--- sphincs.py
+++ sphincs.py
@@ -2,15 +2,20 @@ import pyspx.shake_128f as sphincs
 import os
 
 def read_from_file(filename):
+
+    # open and read the whole plaintext file
     with open(filename, 'r') as f:
         return str(f.read())
 
+# seed urandom with 48 for reliable randomness
 seed = os.urandom(48)
 
+# generate private and shared key using the library
 pk, sk = sphincs.generate_keypair(seed)
 
 i = 0
 
+# Loop until all text files have been encrypted/decrypted
 while i != 100:
     i = i + 1
         
